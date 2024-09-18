c0383f2dcd086a3ce63acad8d4af261a7f4f96f2
rzlftwaa-cloud <rzlftwaastudio@gmail.com>
2024-09-19 00:01:41 +0700

    Create change-ns.sh

---
+++ change-ns.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+oldns=$(cat /etc/xray/dns)
+clear
+echo ""
+echo ""
+read -p " Masukan domain NS yang baru : " newns
+sed -i "s/${oldns}/${newns}/g" /etc/systemd/system/client.service
+rm -f /etc/xray/dns ; echo "${newns}" > /etc/xray/dns
+systemctl restart client server
+echo " NS Domain berhasil di ubah ke $newns "
+sleep 2
+menu
